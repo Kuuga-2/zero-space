16cfef79d6195abe0df0ed1fc41c70476ba84e6e
Season <seasondaimy@foxmail.com>
2025-04-07 09:45:22 +0800

    chore: add react scan

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
+import Script from "next/script"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -20,6 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* 
+      <head>
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />
+      </head> 
+      */}
+      <Script src="https://unpkg.com/react-scan/dist/auto.global.js"></Script>
+      <ReactScan />
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
